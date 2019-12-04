118e9ca0bbebfa6d605d0fb86a5f532210cb2326
captainxyw <captainxyw@gmail.com>
2019-12-04 13:27:12 +0800

    add check cookies

--- src/main/java/com/xuyewei/community/controller/IndexController.java
+++ src/main/java/com/xuyewei/community/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.xuyewei.community.controller;
 
+import com.xuyewei.community.mapper.UserMapper;
+import com.xuyewei.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * ClassName:HelloController
@@ -13,8 +19,25 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
-    @GetMapping("/hello")
-    public String index() {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 
--- src/main/java/com/xuyewei/community/mapper/UserMapper.java
+++ src/main/java/com/xuyewei/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.xuyewei.community.mapper;
 import com.xuyewei.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * ClassName:UserMapper
@@ -18,4 +20,6 @@ public interface UserMapper {
             "values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
