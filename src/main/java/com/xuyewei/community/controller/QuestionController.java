46ef8bf2f644c78c780587e64413f694f8d9b46e
captainxyw <captainxyw@gmail.com>
2019-12-08 23:11:44 +0800

    问题详情页面

---
+++ src/main/java/com/xuyewei/community/controller/QuestionController.java
@@ -0,0 +1,31 @@
+package com.xuyewei.community.controller;
+
+import com.xuyewei.community.dto.QuestionDTO;
+import com.xuyewei.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * ClassName:QuestionController
+ * Package:com.xuyewei.community.controller
+ * Description:
+ *
+ * @Date:2019/12/8 22:19
+ * @Author:xuyewei
+ */
+@Controller
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model) {
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
