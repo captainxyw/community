f782ea214dd5352aeb00c320651c63b67a275d48
captainxyw <captainxyw@gmail.com>
2019-12-14 00:08:55 +0800

    不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,19 +2,27 @@ function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
     $.ajax({
-        type:"post",
-        url:"/comment",
+        type: "post",
+        url: "/comment",
         contentType: "application/json",
-        data:JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
-        success: function(response) {
-            if(response.code == 200) {
+        success: function (response) {
+            if (response.code == 200) {
                 $("comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=6d552a5840c9a09d3616&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
             console.log(response);
         },
