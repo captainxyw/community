063da77a61f11b630ec1ee7babc442de3dddc10f
captainxyw <captainxyw@gmail.com>
2019-12-15 21:01:47 +0800

    优化回复功能和问题详情页面

--- src/main/java/com/xuyewei/community/controller/CommentController.java
+++ src/main/java/com/xuyewei/community/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.xuyewei.community.exception.CustomizeErrorCode;
 import com.xuyewei.community.model.Comment;
 import com.xuyewei.community.model.User;
 import com.xuyewei.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,6 +37,12 @@ public class CommentController {
         if(user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
+
+        if(commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
+
+        }
+
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
         comment.setContent(commentCreateDTO.getContent());
