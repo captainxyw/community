25c955707116251503c30b1cbc5bddbe0213e793
captainxyw <captainxyw@gmail.com>
2019-12-16 21:01:45 +0800

    提问页面相关问题展示

--- src/main/java/com/xuyewei/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/xuyewei/community/mapper/QuestionExtMapper.java
@@ -3,9 +3,12 @@ package com.xuyewei.community.mapper;
 import com.xuyewei.community.model.Question;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface QuestionExtMapper {
 
     int incView(@Param("record") Question record);
     int incCommentCount(@Param("record") Question record);
+    List<Question> selectRelated(Question question);
 
 }
